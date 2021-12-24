c09fbaf73b69142b95081fa8a5e1a6f4ceb683ee
romulo <ferreira.moura.maia@gmail.com>
2021-12-24 14:14:22 -0300

    created api

---
+++ api/handler.py
@@ -0,0 +1,39 @@
+import pickle
+import pandas as pd
+from flask           import Flask, request, Response
+from rossmann.Rossmann import Rossmann
+
+model = pickle.load(open('/home/romulo/Documentos/dsprod/sales_prediction/model/model_rossman_xgb_v01.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/rossmann/predict', methods=['POST'])
+def rossmann_predict():
+    test_json = request.get_json()
+    if test_json:
+        if isinstance(test_json, dict):
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else:
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+            
+        # instanciete Rossman Class    
+        pipeline = Rossmann()
+        # data cleaning
+        df1 = pipeline.data_cleaning(test_raw)
+        #feature engineering
+        df2 = pipeline.feature_engineering(df1)
+        # data preparation
+        df3 = pipeline.data_preparation(df2)
+        # prediction
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+        
+        return df_response
+    
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+    
+    
+    
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
+    
